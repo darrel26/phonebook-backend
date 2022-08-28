59f98b9ff3012b4bd8275e0ca18264ec4f057f8e
Noid <dionisiusgunadi@outlook.com>
2022-08-28 14:17:35 +0700

    add Procfile

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(morgan(':method :url :status :response-time ms :body'));
 app.use('/', services);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
