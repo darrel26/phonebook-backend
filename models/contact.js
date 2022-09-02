cad2f77fbd418a63d55c9186b106c305dc577fc2
Noid <dionisiusgunadi@outlook.com>
2022-09-02 16:13:37 +0700

    fixed build for exercise 3.19 - 3.20

--- models/contact.js
+++ models/contact.js
@@ -22,6 +22,12 @@ const contactSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
+    validate: {
+      validator: function (phoneNum) {
+        return /^\d{2,3}-\d{5,}/.test(phoneNum);
+      },
+      error: (props) => `${props.value} is not a valid phone number!`,
+    },
     required: true,
   },
 });
